83da33de68aad2bef1b0e7a255cf2cb9a8179a8e
tNgan23 <huynhthitrucngantl@gmail.com>
2024-03-13 06:35:42 +0700

    blabla

--- index.php
+++ index.php
@@ -145,7 +145,7 @@
                   <label class="form-label">Date of birth</label>
                   <input type="date" class="form-control shadow-none">
                 </div>
-                
+
               </div>
             </div>
             <div class="text-center my-1">
@@ -157,7 +157,7 @@
     </div>
   </div>
 
-  <!-- Carousel -->
+  <!-- Quảng Cáo -->
 
   <div class="container-fluid px-lg-4 mt-4">
     <div class="swiper swiper-container">
@@ -200,7 +200,7 @@
     </div>
   </div>
 
-  <!-- check availability form -->
+  <!-- Check Booking -->
 
   <div class="container  availability-form">
     <div class="row">
@@ -243,29 +243,92 @@
     </div>
   </div>
 
+  <!-- Rooms -->
+
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMS</h2>
+<!-- 1:11:38 -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 my-3">
+        <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+          <img src="image/rooms/1.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Simple Room Name</h5>
+            <h6 class="mb-4">500$ per night</h6>
+            <div class="features mb-4">
+               <h6 class="mb-1">Features</h6> <!--Các tính năng -->
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                2 Room <!--phòng ngủ -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                1 Bathroom <!--phòng tắm -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                1 Balcony <!--ban công -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                3 Sofa
+              </span>
+            </div>
+            <div class="facilities mb-4">
+              <h6 class="mb-1">Facilities</h6> <!--Các tiện nghi -->
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Wifi
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Televisiom
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                AC <!--Máy lạnh (Air Conditioning) -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Room heater <!--bình nóng lạnh -->
+              </span>
+            </div>
+            <div class="rating mb-4">
+            <h6 class="mb-1">Rating</h6> 
+            <span class="badge rounded-pill bg-light">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </span>
+            </div>
+            <div class="d-flex justify-content-evenly mb-2">
+            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>  
+          </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Romm >>></a>
+      </div>
+    </div>
+
+    <br><br><br>
+    <br><br><br>
 
 
-  <br><br><br>
-  <br><br><br>
 
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"></script>
 
-
-  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
-
-  <script>
-    var swiper = new Swiper(".swiper-container", {
-      spaceBetween: 30,
-      effect: "fade",
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      }
-    });
-  </script>
+    <script>
+      var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        effect: "fade",
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        }
+      });
+    </script>
 
 
 
