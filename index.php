560f6ffcd3ef1d48f6f082c6f554d15b087e4a8b
tNgan23 <huynhthitrucngantl@gmail.com>
2024-03-15 01:50:38 +0700

    init code 02

--- index.php
+++ index.php
@@ -13,15 +13,28 @@
     rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/common.css">
 
+  <style>
+    .availability-form {
+      margin-top: -50px;
+      z-index: 2;
+      position: relative;
+    }
 
+    @media screen and (max-width: 575px) {
+      .availability-form {
+        margin-top: 25px;
+        padding: 0 35px;
+      }
+    }
+  </style>
 </head>
 
 <body class="bg-light">
   <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Hotel Booking</a>
+      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">HOTEL BOOKING</a>
       <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
         aria-label="Toggle navigation">
@@ -243,10 +256,10 @@
     </div>
   </div>
 
-  <!-- Rooms -->
+  <!-- Our Rooms -->
 
   <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMS</h2>
-<!-- 1:11:38 -->
+
   <div class="container">
     <div class="row">
       <div class="col-lg-4 col-md-6 my-3">
@@ -256,7 +269,7 @@
             <h5 class="card-title">Simple Room Name</h5>
             <h6 class="mb-4">500$ per night</h6>
             <div class="features mb-4">
-               <h6 class="mb-1">Features</h6> <!--Các tính năng -->
+              <h6 class="mb-1">Features</h6> <!--Các tính năng -->
               <span class="badge roundes-pill bg-light text-dark text-wrap">
                 2 Room <!--phòng ngủ -->
               </span>
@@ -286,20 +299,127 @@
               </span>
             </div>
             <div class="rating mb-4">
-            <h6 class="mb-1">Rating</h6> 
-            <span class="badge rounded-pill bg-light">
-            <i class="bi bi-star-fill text-warning"></i>
-            <i class="bi bi-star-fill text-warning"></i>
-            <i class="bi bi-star-fill text-warning"></i>
-            <i class="bi bi-star-fill text-warning"></i>
-            <i class="bi bi-star-fill text-warning"></i>
-            </span>
+              <h6 class="mb-1">Rating</h6>
+              <span class="badge rounded-pill bg-light">
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+              </span>
             </div>
             <div class="d-flex justify-content-evenly mb-2">
-            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
-            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>  
+              <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+              <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>
+            </div>
           </div>
-            
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 my-3">
+        <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+          <img src="image/rooms/1.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Simple Room Name</h5>
+            <h6 class="mb-4">500$ per night</h6>
+            <div class="features mb-4">
+              <h6 class="mb-1">Features</h6> <!--Các tính năng -->
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                2 Room <!--phòng ngủ -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                1 Bathroom <!--phòng tắm -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                1 Balcony <!--ban công -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                3 Sofa
+              </span>
+            </div>
+            <div class="facilities mb-4">
+              <h6 class="mb-1">Facilities</h6> <!--Các tiện nghi -->
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Wifi
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Television
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                AC <!--Máy lạnh (Air Conditioning) -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Room heater <!--bình nóng lạnh -->
+              </span>
+            </div>
+            <div class="rating mb-4">
+              <h6 class="mb-1">Rating</h6>
+              <span class="badge rounded-pill bg-light">
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+              </span>
+            </div>
+            <div class="d-flex justify-content-evenly mb-2">
+              <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+              <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 my-3">
+        <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+          <img src="image/rooms/1.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Simple Room Name</h5>
+            <h6 class="mb-4">500$ per night</h6>
+            <div class="features mb-4">
+              <h6 class="mb-1">Features</h6> <!--Các tính năng -->
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                2 Room <!--phòng ngủ -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                1 Bathroom <!--phòng tắm -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                1 Balcony <!--ban công -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                3 Sofa
+              </span>
+            </div>
+            <div class="facilities mb-4">
+              <h6 class="mb-1">Facilities</h6> <!--Các tiện nghi -->
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Wifi
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Televisiom
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                AC <!--Máy lạnh (Air Conditioning) -->
+              </span>
+              <span class="badge roundes-pill bg-light text-dark text-wrap">
+                Room heater <!--bình nóng lạnh -->
+              </span>
+            </div>
+            <div class="rating mb-4">
+              <h6 class="mb-1">Rating</h6>
+              <span class="badge rounded-pill bg-light">
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+              </span>
+            </div>
+            <div class="d-flex justify-content-evenly mb-2">
+              <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+              <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>
+            </div>
           </div>
         </div>
       </div>
@@ -307,32 +427,159 @@
         <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Romm >>></a>
       </div>
     </div>
+  </div>
+  <!-- OUR FACILITIES -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
 
-    <br><br><br>
-    <br><br><br>
+  <div class="container">
+    <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="image/facilities/Wifi.svg" width="80px">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="image/facilities/Television.svg" width="80px">
+        <h5 class="mt-3">Television</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="image/facilities/Radio.svg" width="80px">
+        <h5 class="mt-3">Radio Music</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="image/facilities/AC.svg" width="80px">
+        <h5 class="mt-3">Air Conditioner</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="image/facilities/WaterHeater.svg" width="80px">
+        <h5 class="mt-3">Water Heater</h5>
+      </div>
+      <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- TESTIMONIALS -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
+  <div class="container">
+    <div class="swiper swiper-testimonials">
+      <div class="swiper-wrapper mb-5">
+        <div class="swiper-slide bg-white p-4">
+          <div class="profile d-flex align-items-center mb-3">
+            <img src="image/facilities/star.svg" width="30px">
+            <h6 class="m-0 ms-2">Random user1</h6>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nobis placeat, quidem adipisci, explicabo ratione deleniti minima ab veritatis vel, doloremque incidunt!
+            Vel rem sapiente ipsam explicabo eum consequatur incidunt voluptate.
+          </p>
+          <div class="rating">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+          </div>
+        </div>
+        <div class="swiper-slide bg-white p-4">
+          <div class="profile d-flex align-items-center mb-3">
+            <img src="image/facilities/star.svg" width="30px">
+            <h6 class="m-0 ms-2">Random user2</h6>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nobis placeat, quidem adipisci, explicabo ratione deleniti minima ab veritatis vel, doloremque incidunt!
+            Vel rem sapiente ipsam explicabo eum consequatur incidunt voluptate.
+          </p>
+          <div class="rating">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+          </div>
+        </div>
+        <div class="swiper-slide bg-white p-4">
+          <div class="profile d-flex align-items-center mb-3">
+            <img src="image/facilities/star.svg" width="30px">
+            <h6 class="m-0 ms-2">Random user3</h6>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nobis placeat, quidem adipisci, explicabo ratione deleniti minima ab veritatis vel, doloremque incidunt!
+            Vel rem sapiente ipsam explicabo eum consequatur incidunt voluptate.
+          </p>
+          <div class="rating">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+
+  </div>
+
+
+  <br><br><br>
+  <br><br><br>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-      crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
 
-    <script>
-      var swiper = new Swiper(".swiper-container", {
-        spaceBetween: 30,
-        effect: "fade",
-        loop: true,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-        }
-      });
-    </script>
-
-
-
-
+  <script>
+    var swiper = new Swiper(".swiper-container", {
+      spaceBetween: 30,
+      effect: "fade",
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      }
+    });
+    var swiper = new Swiper(".swiper-testimonials", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      slidesPerView: "3",
+      loop: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3, // Giả sử bạn muốn hiển thị 3 slides tại key 1024
+        },
+        
+      }
+    });
+  </script>
 </body>
 
 </html>
