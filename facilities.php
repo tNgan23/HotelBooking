76f3f7a7d8da13f88867e598523ce52ffee5a9bc
tNgan23 <huynhthitrucngantl@gmail.com>
2024-03-23 03:23:06 +0700

    1:1;05

--- facilities.php
+++ facilities.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hotel Booking-FACILITIES</title>
+  <title>Hotel Booking - FACILITIES</title>
   <?php require ('inc/links.php'); ?>
   <style>
     :root {
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hotel Booking-HOME</title>
+  <title>Hotel Booking - HOME</title>
   <?php require ('inc/links.php'); ?>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
