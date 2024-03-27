729ccb558733799cae178d82b536522042ecbbaa
vuphucloc789 <phuclocdz87@gmail.com>
2024-03-27 09:28:18 +0700

    sua c8

--- Admin/settings.php
+++ Admin/settings.php
@@ -66,8 +66,7 @@ adminLogin();
                                     <button type="button"
                                         onclick="site_title.value = general_data.site_title, site_about.value =  general_data.site_about"
                                         class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="submit"
-                                        class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
                                 </div>
                             </div>
                         </form>
@@ -119,11 +118,11 @@ adminLogin();
                                     <h6 class="card-subtitle mb-1 fw-bold">Phone Numbers</h6>
                                     <p class="card-text mb-1">
                                         <id="<i class="bi bi-telephone-fill"></i>
-                                        <span id="pn1"></span>
+                                            <span id="pn1"></span>
                                     </p>
                                     <p class="card-text">
                                         <id="<i class="bi bi-telephone-fill"></i>
-                                        <span id="pn2"></span>
+                                            <span id="pn2"></span>
                                     </p>
                                 </div>
                                 <div class="mb-4">
@@ -151,15 +150,15 @@ adminLogin();
                                     <h6 class="card-subtitle mb-1 fw-bold">iFrame</h6>
                                     <iframe id="iframe" class="border p-2 w-100" loading="lazy"></iframe>
                                 </div>
-                                
+
                             </div>
                         </div>
-                        
+
 
 
                     </div>
                 </div>
-            
+
                 <!-- Contacts details modal -->
 
                 <div class="modal fade" id="contacts-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
@@ -185,14 +184,21 @@ adminLogin();
                                                         class="form-control shadow-none" required>
                                                 </div>
                                                 <div class="mb-3">
-                                                    <label class="form-label fw-bold">Phone Numbers (with country code) </label>
+                                                    <label class="form-label fw-bold">Phone Numbers (with country code)
+                                                    </label>
                                                     <div class="input-group mb-3">
-                                                        <span class="input-group-text"><id="<i class="bi bi-telephone-fill"></i></span>
-                                                        <input type="text" name="pn1" id="pn1_inp" class="form-control shadow-none" require>
+                                                        <span class="input-group-text">
+                                                            <id="<i class="bi bi-telephone-fill"></i>
+                                                        </span>
+                                                        <input type="text" name="pn1" id="pn1_inp"
+                                                            class="form-control shadow-none" require>
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <span class="input-group-text"><id="<i class="bi bi-telephone-fill"></i></span>
-                                                        <input type="text" name="pn2" id="pn2_inp" class="form-control shadow-none">
+                                                        <span class="input-group-text">
+                                                            <id="<i class="bi bi-telephone-fill"></i>
+                                                        </span>
+                                                        <input type="text" name="pn2" id="pn2_inp"
+                                                            class="form-control shadow-none">
                                                     </div>
                                                 </div>
                                                 <div class="mb-3">
@@ -205,16 +211,22 @@ adminLogin();
                                                 <div class="mb-3">
                                                     <label class="form-label fw-bold">Social Links</label>
                                                     <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i class="bi bi-facebook"></i></span>
-                                                        <input type="text" name="fb" id="fb_inp" class="form-control shadow-none" require>
+                                                        <span class="input-group-text"><i
+                                                                class="bi bi-facebook"></i></span>
+                                                        <input type="text" name="fb" id="fb_inp"
+                                                            class="form-control shadow-none" required>
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i class="bi bi-instagram"></i></span>
-                                                        <input type="text" name="insta" id="insta_inp" class="form-control shadow-none"require>
+                                                        <span class="input-group-text"><i
+                                                                class="bi bi-instagram"></i></span>
+                                                        <input type="text" name="insta" id="insta_inp"
+                                                            class="form-control shadow-none" required>
                                                     </div>
                                                     <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i class="bi bi-youtube"></i></span>
-                                                        <input type="text" name="ytb" id="ytb_inp" class="form-control shadow-none">
+                                                        <span class="input-group-text"><i
+                                                                class="bi bi-youtube"></i></span>
+                                                        <input type="text" name="ytb" id="ytb_inp"
+                                                            class="form-control shadow-none">
                                                     </div>
                                                 </div>
                                                 <div class="mb-3">
@@ -229,8 +241,7 @@ adminLogin();
                                 <div class="modal-footer">
                                     <button type="button" onclick="contacts_inp(contacts_data)"
                                         class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="submit"
-                                        class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
                                 </div>
                             </div>
                         </form>
@@ -253,8 +264,7 @@ adminLogin();
     let contacts_s_form = document.getElementById('contacts_s_form');
 
 
-    function get_general() 
-    {
+    function get_general() {
         let site_title = document.getElementById('site_title');
         let site_about = document.getElementById('site_about');
 
@@ -286,7 +296,7 @@ adminLogin();
         xhr.send('get_general');
     }
 
-    general_s_form.addEventListener('submit', function(e){
+    general_s_form.addEventListener('submit', function(e) {
         e.preventDefault();
         upd_genaral(site_title_inp.value, site_about_inp.value)
     })
@@ -319,11 +329,9 @@ adminLogin();
 
         xhr.onload = function() {
 
-            if (this.responseText == 1 && general_data.shutdown == 0) 
-            {
+            if (this.responseText == 1 && general_data.shutdown == 0) {
                 alert('success', 'Site has been shutdown !  ');
-            } else 
-            {
+            } else {
                 alert('error', 'Shutdown mode off !   ');
             }
             get_general();
@@ -333,9 +341,8 @@ adminLogin();
 
     }
 
-    function get_contacts() 
-    {
-        let contacts_p_id = ['address','gmap','pn1','pn2','email','fb','insta','ytb'];
+    function get_contacts() {
+        let contacts_p_id = ['address', 'gmap', 'pn1', 'pn2', 'email', 'fb', 'insta', 'ytb'];
         let iframe = document.getElementById('iframe');
 
         let xhr = new XMLHttpRequest();
@@ -346,8 +353,8 @@ adminLogin();
             contacts_data = JSON.parse(this.responseText);
             contacts_data = Object.values(contacts_data);
 
-            for(i=0;i<contacts_p_id.length;i++){
-                document.getElementById(contacts_p_id[i]).innerText = contacts_data[i+1];
+            for (i = 0; i < contacts_p_id.length; i++) {
+                document.getElementById(contacts_p_id[i]).innerText = contacts_data[i + 1];
             }
             iframe.src = contacts_data[9];
             contacts_inp(contacts_data);
@@ -356,30 +363,31 @@ adminLogin();
         xhr.send('get_contacts');
     }
 
-    function contacts_inp(data)
-    {
-        let contacts_inp_id = ['address_inp','gmap_inp','pn1_inp','pn2_inp','email_inp','fb_inp','insta_inp','ytb_inp','iframe_inp'];
+    function contacts_inp(data) {
+        let contacts_inp_id = ['address_inp', 'gmap_inp', 'pn1_inp', 'pn2_inp', 'email_inp', 'fb_inp', 'insta_inp',
+            'ytb_inp', 'iframe_inp'
+        ];
 
-        for(i=0;i<contacts_inp_id.length;i++)
-        {
-            document.getElementById(contacts_inp_id[i]).value = data[i+1];
+        for (i = 0; i < contacts_inp_id.length; i++) {
+            document.getElementById(contacts_inp_id[i]).value = data[i + 1];
         }
     }
 
-    contacts_s_form.addEventListener('submit', function(e){
+    contacts_s_form.addEventListener('submit', function(e) {
         e.preventDefault();
         upd_contacts();
     })
 
-    function upd_contacts()
-    {
-        let index = ['address','gmap','pn1','pn2','email','fb','insta','ytb','iframe'];
-        let contacts_inp_id = ['address_inp','gmap_inp','pn1_inp','pn2_inp','email_inp','fb_inp','insta_inp','ytb_inp','iframe_inp'];
-        
-        let data_str="";
-        
-        for(i=0;i<index.length;i++){
-            data_str += index[i]+ "=" + document.getElementById(contacts_inp_id[i]).value + '&';
+    function upd_contacts() {
+        let index = ['address', 'gmap', 'pn1', 'pn2', 'email', 'fb', 'insta', 'ytb', 'iframe'];
+        let contacts_inp_id = ['address_inp', 'gmap_inp', 'pn1_inp', 'pn2_inp', 'email_inp', 'fb_inp', 'insta_inp',
+            'ytb_inp', 'iframe_inp'
+        ];
+
+        let data_str = "";
+
+        for (i = 0; i < index.length; i++) {
+            data_str += index[i] + "=" + document.getElementById(contacts_inp_id[i]).value + '&';
         }
         data_str += "upd_contacts";
 
@@ -387,16 +395,14 @@ adminLogin();
         xhr.open("POST", "ajax/settings_crud.php", true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-        xhr.onload = function(){
+        xhr.onload = function() {
             var myModal = document.getElementById('contacts-s');
             var modal = bootstrap.Modal.getInstance(myModal);
             modal.hide();
-            if (this.responseText == 1 ) 
-            {
-                alert('success','Changes saved!');
+            if (this.responseText == 1) {
+                alert('success', 'Changes saved!');
                 get_contacts();
-            } else 
-            {
+            } else {
                 alert('error', 'No changes made!');
             }
             get_general();
